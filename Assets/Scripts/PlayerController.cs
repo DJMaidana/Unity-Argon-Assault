8d401d3bf1d5c6cf9b6bca48e31bcabb1158010d
Damian Maidana <damian.j.maidana@gmail.com>
2022-07-22 21:23:11 -0300

    Added pitch, yaw and roll response to movements

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,54 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 2f;
+    [SerializeField] float minXRange = 10f;
+    [SerializeField] float maxXRange = 10f;
+    [SerializeField] float minYRange = 10f;
+    [SerializeField] float maxYRange = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float positionRollFactor = -20f;
 
+    [SerializeField] float pitchIntensity = -10f;
+    
+
+    float xInput, yInput;
     // Update is called once per frame
     void Update()
     {
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
+        MoveShip();
+        RotateShip();
+    }
 
-        Vector3 localOffset = new Vector3(xInput,yInput,0) * Time.deltaTime * movementSpeed;
+    void MoveShip()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
 
-        transform.localPosition = transform.localPosition + localOffset;     
+        Vector3 localOffset = new Vector3(xInput, yInput, 0) * Time.deltaTime * movementSpeed;
+
+        Vector3 newLocalPos = transform.localPosition + localOffset; 
+
+        float clampedXPos = Mathf.Clamp(newLocalPos.x, minXRange, maxXRange);   // Clamps values to prevent going off screen
+        float clampedYPos = Mathf.Clamp(newLocalPos.y, minYRange, maxYRange);
+
+        Vector3 clampedLocalPos = new Vector3(clampedXPos, clampedYPos, newLocalPos.z);
+
+        transform.localPosition = clampedLocalPos;
+    }
+
+    void RotateShip()
+    {
+        transform.localRotation = Quaternion.Euler(-30f, 30f, 0f);
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yInput * pitchIntensity;
+        
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xInput * positionRollFactor;
+        
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+
     }
 }

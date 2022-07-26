cd4b1d2916c4eecf504f95b861cc98048bfc8add
Damian Maidana <damian.j.maidana@gmail.com>
2022-07-26 19:28:48 -0300

    Added canvas with simple UI for score

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     float score;
+    TMP_Text scoreText;
+
+    void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = "0";
+    }
 
     public void IncreaseScore(int scoreAwarded)
     {
         score += scoreAwarded;
-        Debug.Log("Score: " + score);
+        scoreText.text = score.ToString();
     }
 }

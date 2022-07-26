67ef138eeb9c9b833e6b2b692e2cecaec8de0c92
Damian Maidana <damian.j.maidana@gmail.com>
2022-07-26 20:49:48 -0300

    Added new enemy prefabs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] int scoreAwarded;
+    [SerializeField] int scoreOnHit = 1;
+    [SerializeField] int scoreOnDestroy = 1;
+    [SerializeField] int hitPoints = 1;
 
     [SerializeField] GameObject vfx_EnemyExplosion;
+    [SerializeField] GameObject vfx_EnemyHit;
     [SerializeField] Transform parent;
 
     ScoreBoard scoreBoard;
 
     void Start()
     {
+        gameObject.AddComponent<Rigidbody>();
+        GetComponent<Rigidbody>().useGravity = false;
         scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     void OnParticleCollision(GameObject other)
     {
-        scoreBoard.IncreaseScore(scoreAwarded);
-        Instantiate(vfx_EnemyExplosion, transform.position, transform.rotation);
-        Destroy(gameObject);
+        if (hitPoints > 0)
+        {
+            GameObject vfx = Instantiate(vfx_EnemyHit, transform.position, transform.rotation);
+            vfx.transform.parent = parent;
+            scoreBoard.IncreaseScore(scoreOnHit);
+            hitPoints -= 1;
+        }
+        else
+        {
+            scoreBoard.IncreaseScore(scoreOnDestroy);            
+            GameObject vfx = Instantiate(vfx_EnemyExplosion, transform.position, transform.rotation);
+            vfx.transform.parent = parent;
+            Destroy(gameObject);
+        }
     }
 }

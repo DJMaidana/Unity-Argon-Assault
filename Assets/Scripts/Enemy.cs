c3546590e5ad861944c6f47924d3ad3226d0866d
Damian Maidana <damian.j.maidana@gmail.com>
2022-07-26 21:26:16 -0300

    Fixed issue where VFX were cluttering the Hierarchy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,32 +10,44 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] GameObject vfx_EnemyExplosion;
     [SerializeField] GameObject vfx_EnemyHit;
-    [SerializeField] Transform parent;
+    [SerializeField] GameObject parentGameObject;
 
     ScoreBoard scoreBoard;
 
     void Start()
     {
-        gameObject.AddComponent<Rigidbody>();
-        GetComponent<Rigidbody>().useGravity = false;
-        scoreBoard = FindObjectOfType<ScoreBoard>();
+        gameObject.AddComponent<Rigidbody>();               //  Add a Rigidbody to the GameObject
+        GetComponent<Rigidbody>().useGravity = false;       //  Disable gravity so it doesn't fall
+        scoreBoard = FindObjectOfType<ScoreBoard>();        //  Get reference to scoreboard GameObject
+
+        parentGameObject = GameObject.FindWithTag("Spawn Collector");       //  Get the transform of tag Spawn Collector
     }
 
     void OnParticleCollision(GameObject other)
     {
         if (hitPoints > 0)
         {
-            GameObject vfx = Instantiate(vfx_EnemyHit, transform.position, transform.rotation);
-            vfx.transform.parent = parent;
-            scoreBoard.IncreaseScore(scoreOnHit);
-            hitPoints -= 1;
+            EnemyHit();
         }
         else
         {
-            scoreBoard.IncreaseScore(scoreOnDestroy);            
-            GameObject vfx = Instantiate(vfx_EnemyExplosion, transform.position, transform.rotation);
-            vfx.transform.parent = parent;
-            Destroy(gameObject);
+            EnemyDestroy();
         }
+    }
+
+    void EnemyHit()
+    {
+        GameObject vfx = Instantiate(vfx_EnemyHit, transform.position, transform.rotation);
+        vfx.transform.parent = parentGameObject.transform;
+        scoreBoard.IncreaseScore(scoreOnHit);
+        hitPoints -= 1;
+    }
+    
+    void EnemyDestroy()
+    {
+        scoreBoard.IncreaseScore(scoreOnDestroy);
+        GameObject vfx = Instantiate(vfx_EnemyExplosion, transform.position, transform.rotation);
+        vfx.transform.parent = parentGameObject.transform;
+        Destroy(gameObject);
     }
 }
